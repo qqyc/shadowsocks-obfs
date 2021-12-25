9176f9874e719ea5dd3e59c665d7f0ddd17caa0a
lafawkes <85199979+lafawkes@users.noreply.github.com>
2021-12-25 20:17:58 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@
 PASSWORD=${PASSWORD:-"obfspasswd"}
 METHOD=${METHOD:-"chacha20-ietf-poly1305"}
 PORT=${PORT:-8888}
+OBFS_TYPE=${OBFS_TYPE:-"http"}
 
 
 # print configs
@@ -15,6 +16,7 @@ echo "port: $PORT"
 
 # modify config files
 sed -i "s/obfspasswd/$PASSWORD/g" /etc/shadowsocks-libev/config.json
+sed -i "s/http/$OBFS_TYPE/g" /etc/shadowsocks-libev/config.json
 sed -i "s/chacha20-ietf-poly1305/$METHOD/g" /etc/shadowsocks-libev/config.json
 sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
 rm /etc/nginx/sites-enabled/default
